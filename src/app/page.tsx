5edfa0e1cb1f11e62b77649fc0900f4b1606829d
Ayaan Zeeshan <muhammad.ayaan.zeeshan123@gmail.com>
2024-12-18 13:25:20 +0500

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 // useState in Next.js
 
-// import State from "./component/State";
+import State from "./component/State";
 // import State1 from "./component/State1";
 // import Car from "./component/State1";
 // import Car1 from "./component/State1";
-import Car2 from "./component/State1";
+// import Car2 from "./component/State1";
 
 export default function Home() {
   return (
     <div>
-      {/* <State /> */}
+      <State />
       {/* <State1/> */}
       {/* <Car/> */}
       {/* <Car1/> */}
-      <Car2/>
+      {/* <Car2/> */}
     </div>
   );
 }
